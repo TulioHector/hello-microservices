21e035d7718df24977b6f42aa38ed5dbb8471696
Ravi Srinivasan <rsriniva@redhat.com>
2018-02-20 09:17:30 +0530

    Solution branch for GE 2.4

--- hola/Readme.md
+++ hola/Readme.md
@@ -1,11 +1,11 @@
 # hola
 hola microservice using MicroProfile on WildFly Swarm
 
-View generated OpenShift resources
+Deploy the application in OpenShift
 -----------------------------------
 
 1. Make sure to be logged into OpenShift.
 2. Make sure you have created or selected the appropriate OpenShift project. 
 3. Execute:
 
-        mvn clean fabric8:resource
+        mvn clean fabric8:deploy
---
+++ hola/src/test/java/com/redhat/training/msa/hola/rest/HolaResourceFallbackTest.java
@@ -0,0 +1,40 @@
+package com.redhat.training.msa.hola.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.arquillian.CreateSwarm;
+
+import com.redhat.training.msa.hola.ArquillianTestUtils;
+
+@RunWith(Arquillian.class)
+public class HolaResourceFallbackTest {
+
+	
+	@Deployment
+	public static WebArchive createWebArchive() {
+		return ArquillianTestUtils.deploy().addAsManifestResource("META-INF/microprofile-config.properties","microprofile-config.properties");
+	}
+	
+
+	@CreateSwarm
+	public static Swarm newContainer() throws Exception {
+		return ArquillianTestUtils.newContainer();
+	}
+
+
+    @Test
+    @RunAsClient
+    public void testHola() throws Exception {
+    	given().when().get("/api/hola").then().body(containsString("Hola de localhost"));
+    }
+
+
+}
