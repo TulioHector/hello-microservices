5010cd237ce91c2c3cea2067831546ad3ae4a4b3
Ricardo Jun <jtaniguc@redhat.com>
2018-02-22 11:30:48 -0300

    Included the test used by chapter 4 GE

--- hola/src/test/java/com/redhat/training/msa/hola/rest/HolaResourceFallBackIntegrationTest.java
+++ hola/src/test/java/com/redhat/training/msa/hola/rest/HolaResourceFallBackIntegrationTest.java
@@ -1,17 +1,6 @@
 package com.redhat.training.msa.hola.rest;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-
 import java.net.URL;
-import java.util.Set;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -21,8 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.arquillian.CreateSwarm;
-
-import com.redhat.training.msa.hola.ArquillianTestUtils;
 
 
 @RunWith(Arquillian.class)
@@ -35,26 +22,22 @@ public class HolaResourceFallBackIntegrationTest {
 	@Deployment
 	public static WebArchive deploy() {
 		//TODO Delegate a call to the deploy static method from ArquillianTestUtils class
-		return ArquillianTestUtils.deploy().addAsManifestResource("META-INF/microprofile-config.properties","microprofile-config.properties");
+		//TODO Add the microprofile-config.properties file to the META-INF directory
+		return null;
 	}
 	
 	@CreateSwarm
 	public static Swarm createSwarm() throws Exception {
 		//TODO Delegate a call to the newContainer static method from ArquillianTestUtils class
-		return ArquillianTestUtils.newContainer();
+		return null;
 	}
 	@Test
 	public void testFallback() {
 		//TODO Use the ClientBuilder class from javax.ws.rs.client.ClientBuilder class
-		final Client client = ClientBuilder.newBuilder().build();
 		//TODO Define call the target method and pass this.url.toExternalForm()+"/api/hola" as a parameter
-        WebTarget target = client.target(this.url.toExternalForm()+"/api/hola");
 		//TODO The REST Endpoint returns only text, set the request to get MediaType.TEXT_PLAIN and store the output to a Response object
-        Response response = target.request(MediaType.TEXT_PLAIN).get();
 		//TODO Use Hamcrest to evaluate the HTTP code is 200        
-		assertThat(response.getStatus(), is(200));
 		//TODO Use Hamcrest to evaluate the Body of the REST response with "Hola de localhost"
-		assertThat(response.readEntity(String.class), is("Hola de localhost"));
 	}
 
 }
