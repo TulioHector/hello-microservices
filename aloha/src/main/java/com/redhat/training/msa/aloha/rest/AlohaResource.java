a6b5c7db18132180906b73781028d13a3e0055e2
Richard Allred <richardmallred@gmail.com>
2018-04-17 08:00:41 -0400

    Update AlohaResource.java
    Fixing missing improts

--- aloha/src/main/java/com/redhat/training/msa/aloha/rest/AlohaResource.java
+++ aloha/src/main/java/com/redhat/training/msa/aloha/rest/AlohaResource.java
@@ -15,14 +15,16 @@
  */
 package com.redhat.training.msa.aloha.rest;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
