3c188fb3b756daecf36edc10e62d5ac243b20b64
Richard Allred <richardmallred@gmail.com>
2018-04-17 07:43:12 -0400

    Update JaxRsActivator.java

--- aloha/src/main/java/com/redhat/training/msa/aloha/rest/JaxRsActivator.java
+++ aloha/src/main/java/com/redhat/training/msa/aloha/rest/JaxRsActivator.java
@@ -27,8 +27,10 @@ import javax.ws.rs.core.Application;
  * Resources are served relative to the servlet path specified in the {@link ApplicationPath} annotation.
  * </p>
  */
-//TODO Enable JaxRs by extending the Application superclass
+
 //TODO Set a root path of '/api' for the entire application
+
+//TODO Enable JaxRs by extending the Application superclass
 public class JaxRsActivator{
     /* class body intentionally left blank */
 }
