46540e29bd055f1e2c8f7b43e1b731c53ebb8173
Ravi Srinivasan <rsriniva@redhat.com>
2018-05-23 13:59:51 +0530

    Improvements for GE 2.2

--- hola/src/test/java/com/redhat/training/msa/hola/rest/HolaResourceFallbackTest.java
+++ hola/src/test/java/com/redhat/training/msa/hola/rest/HolaResourceFallbackTest.java
@@ -3,31 +3,47 @@ package com.redhat.training.msa.hola.rest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
+import java.io.File;
+import java.util.Properties;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.arquillian.CreateSwarm;
 
-import com.redhat.training.msa.hola.ArquillianTestUtils;
-
 @RunWith(Arquillian.class)
 public class HolaResourceFallbackTest {
 
-	
-	@Deployment
-	public static WebArchive createWebArchive() {
-		return ArquillianTestUtils.deploy().addAsManifestResource("META-INF/microprofile-config.properties","microprofile-config.properties");
-	}
+
+    @Deployment
+    public static Archive<?> createWebArchive() {
+    	
+    	File[] deps = Maven.resolver()
+				.loadPomFromFile("pom.xml")
+				.importDependencies(ScopeType.COMPILE,ScopeType.RUNTIME,ScopeType.TEST).resolve()
+				.withTransitivity().asFile();
+		WebArchive webArchive = ShrinkWrap.create(WebArchive.class, "test.war")
+				.addPackages(true, "com.redhat.training.msa")
+				.addAsLibraries(deps)
+				.addAsResource("project-defaults.yml","project-defaults.yml");		
+		return webArchive;
+    }
 	
 
 	@CreateSwarm
-	public static Swarm newContainer() throws Exception {
-		return ArquillianTestUtils.newContainer();
-	}
+    public static Swarm newContainer() throws Exception {
+        Properties properties = new Properties();
+        properties.put("swarm.http.port", 8080);
+        return new Swarm(properties).withProfile("defaults");
+    }
 
 
     @Test
