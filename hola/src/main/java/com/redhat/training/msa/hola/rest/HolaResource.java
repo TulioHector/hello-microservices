e7d70ca9625d2f4a05b07762d8b623b35478a28d
Ravi Srinivasan <rsriniva@redhat.com>
2018-05-25 11:11:31 +0530

    Fixes for GE 2.2 post defect suggestions

--- hola/src/main/java/com/redhat/training/msa/hola/rest/HolaResource.java
+++ hola/src/main/java/com/redhat/training/msa/hola/rest/HolaResource.java
@@ -7,14 +7,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Path("/")
 @ApplicationScoped
 public class HolaResource {
-
-    private static final Logger LOG = LoggerFactory.getLogger(HolaResource.class);
 
     @Context
     private HttpServletRequest servletRequest;
@@ -30,8 +26,6 @@ public class HolaResource {
     // TODO: Configure method to accept GET requests at the /hola path and return plain text response
     public String hola() {
         String hostname = servletRequest.getServerName();
-        LOG.info("Value in variable a => "+a);
-        LOG.info("Value in variable b => "+b);
-        return String.format("Hola de %s\n", hostname);
+        return String.format("Hola de %s\nValue in property 'a' => %s\nValue in property 'b' => %s\n", hostname,a,b);
     }
 }
