d84786317194e05d68e393b0884a1b8983b93b1d
Ravi Srinivasan <rsriniva@redhat.com>
2018-06-12 17:40:32 +0530

    Re-factor solution branch for Lab 1.7 for RHOAR update

--- aloha/src/main/java/com/redhat/training/msa/aloha/rest/AlohaResource.java
+++ aloha/src/main/java/com/redhat/training/msa/aloha/rest/AlohaResource.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2016, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.redhat.training.msa.aloha.rest;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,21 +6,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
 @Path("/")
-@Api("aloha")
 public class AlohaResource {
 
     @Context
     private HttpServletRequest servletRequest;
 
-
     @GET
     @Path("/aloha")
     @Produces("text/plain")
-    @ApiOperation("Returns the greeting in Hawaiian")
     public String hola() {
         String hostname = servletRequest.getServerName(); 
         return String.format("Aloha mai %s\n", hostname);
