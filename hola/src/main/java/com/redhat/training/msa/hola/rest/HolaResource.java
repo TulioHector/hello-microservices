3edcee1fbcfd2ea821b72f24999aa02e24aae021
Ravi Srinivasan <rsriniva@redhat.com>
2018-06-12 17:19:51 +0530

    Re-factored code for GE 1.6 for RHOAR update

--- hola/src/main/java/com/redhat/training/msa/hola/rest/HolaResource.java
+++ hola/src/main/java/com/redhat/training/msa/hola/rest/HolaResource.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2016, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.redhat.training.msa.hola.rest;
 
 import java.util.ArrayList;
@@ -25,23 +10,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
 @Path("/")
-@Api("hola")
 public class HolaResource {
 
     @Context
     private HttpServletRequest servletRequest;
 
-    /* (non-Javadoc)
-	 * @see com.redhat.training.msa.hola.rest.HolaResource#hola()
-	 */
-	@GET
+	  @GET
     @Path("/hola")
     @Produces("text/plain")
-    @ApiOperation("Returns the greeting in Spanish")
     public String hola() {
         String hostname = servletRequest.getServerName();
         return String.format("Hola de %s\n", hostname);
