68267d217a2c1ef675919ac3089b770dad0ec16b
Fernando Lozano <flozano@redhat.com>
2018-07-12 16:58:44 -0300

    Remove unused imports

--- hola/src/main/java/com/redhat/training/msa/hola/rest/HolaResource.java
+++ hola/src/main/java/com/redhat/training/msa/hola/rest/HolaResource.java
@@ -11,9 +11,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.SecurityContext;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
